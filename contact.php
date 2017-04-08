fa7632639799b37f737436a86c61970169d2f9e7
Amanina Hadi <nabdulha@cse.unl.edu>
2017-04-07 23:18:00 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,28 +1,5 @@
-<?php
-
-if(isset($_POST['send'])) {
-   // Prepare the email
-$to = 'amanina_hadi@yahoo.com';
-
-$name = $_POST['demo-name'];
-$mail_from = $_POST['demo-email'];
-   $subject = 'Message sent from website';
-   $message = $_POST['demo-message'];
-
-$header = "From: $name <$mail_from>";
-
-   // Send it
-   $sent = mail($to, $subject, $message, $header);
-   if($sent) {
-   echo 'Your message has been sent successfully!';
-   } else {
-   echo 'Sorry, your message could not send.';
-   }
-}
-?>
----------------------
-
  <?php 
+if(isset($_POST['send'])) {
      $to ='amanina_hadi@yahoo.com';
      $from =  $_POST['demo-email'];
      $subject = 'Message from Website';
@@ -31,10 +8,10 @@ $header = "From: $name <$mail_from>";
     $headers .= "MIME-Version: 1.0\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1\n";
     if(mail($to, $subject, $message, $headers)){
-        echo 'ok';
+        echo 'Your message has been sent successfully!';
     }
     else{
-        echo 'error';
+        echo 'Sorry, your message could not send.';
     }
-
+}
 ?>
