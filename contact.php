64f88d121c021bd207403cecd43fdea04c487982
Amanina Hadi <nabdulha@cse.unl.edu>
2017-04-07 23:13:02 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -20,3 +20,21 @@ $header = "From: $name <$mail_from>";
    }
 }
 ?>
+---------------------
+
+ <?php 
+     $to ='amanina_hadi@yahoo.com';
+     $from =  $_POST['demo-email'];
+     $subject = 'Message from Website';
+     $message = $_POST['demo-message'];
+    $headers = "From: $from\n";
+    $headers .= "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+    if(mail($to, $subject, $message, $headers)){
+        echo 'ok';
+    }
+    else{
+        echo 'error';
+    }
+
+?>
